2ef6830d3050cb50c801e099ecc1a9aa0bd0c461
pink-cypher <143744116+pink-cypher@users.noreply.github.com>
2024-04-04 17:13:09 -0600

    first led

---
+++ project/1-LED/README.md
@@ -0,0 +1,7 @@
+## Description
+
+* Change the  sequence to dim-to-bright
+
+* Simultaneously bink the red & green leds with independent brightness patterns.
+
+* Change the speed that the brighness patterns change (faster or slower).
---
+++ project/1-LED/main.c
@@ -0,0 +1,54 @@
+//Alternate LEDs from Off, Green, and Red
+#include <msp430.h>
+#include "libTimer.h"
+#include "led.h"
+
+int main(void) {
+  P1DIR |= LEDS;
+  P1OUT &= ~LED_GREEN;
+  P1OUT |= LED_RED;
+
+  configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
+  enableWDTInterrupts();	/* enable periodic interrupt */
+  
+  or_sr(0x18);			/* CPU off, GIE on */
+}
+
+// global state vars that control blinking
+int greenBlinkLimit = 5;  // duty cycle = 1/greenBlinkLimit
+int greenBlinkCount = 0;  // cycles 0...greenBlinkLimit-1
+int redBlinkLimit = 5;  // duty cycle = 1/redBlinkLimit
+int redBlinkCount = 0;  // cycles 0...redBlinkLimit-1
+int brightness = 0; // brightness level
+
+
+void
+__interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
+{
+  // handles green LED blinking 
+  greenBlinkCount ++;
+  if (greenBlinkCount >= greenBlinkLimit) { // on for 1 interrupt period
+    greenBlinkCount = 0;
+    if (brightness >= greenBlinkLimit)
+      P1OUT &= ~LED_GREEN; //full off
+    else if (brightness > 0)
+            P1OUT |= LED_GREEN; // fully on
+  }
+
+   // handle red LED blinking
+   redBlinkCount++;
+   if (redBlinkCount >= redBlinkLimit) { // on for 1 interrupt period
+     redBlinkCount = 0;
+     if (brightness >= redBlinkLimit)
+       P1OUT &= ~LED_RED; // fully off
+     else if (brightness > 0)
+       P1OUT |= LED_RED; // fully on
+   }
+    
+
+    // update brightness level
+    brightness++;
+    if (brightness >= greenBlinkLimit) {
+        brightness = 0;
+    }
+}
